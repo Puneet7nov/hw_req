2f4efbf453ee82fad86927f79aeb91c03edcf0c6
Puneet7nov <40247505+Puneet7nov@users.noreply.github.com>
2018-10-05 19:35:11 +0530

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,37 @@
-
 // If comparable Platform is:
 // No --> Show Platform Size dropdown
 // Yes --> Show list of Existing Platforms in IOP DB
 
-function showhideComparablePlatform(isCompPlatform) {
-    if (isCompPlatform == "Yes") {
-        document.getElementById("platformSize").style.display = 'block';
-        document.getElementById("platForEstimate").style.display = 'none';
-    } 
-    if (isCompPlatform == "No") {
-        document.getElementById("platForEstimate").style.display = 'block';
+$(document).ready(function() {
+      $("input[name='compPlatform']").on('change',function() {
+  //  if (isCompPlatform == "Yes") {
+      if ($(this).val() == "Yes") {
         document.getElementById("platformSize").style.display = 'none';
+        document.getElementById("platForEstimate").style.display = 'block';
+    } 
+ //   if (isCompPlatform == "No") {
+     if ($(this).val() == "No") {
+        document.getElementById("platForEstimate").style.display = 'none';
+        document.getElementById("platformSize").style.display = 'block';
     }
-}
-
+   });
 
 // If Multi Location platform selected --> show List with number of locations
 // and output table should show location column
-// Else if single location --> Do NOT show location column in   
+// Else if single location --> Do NOT show location column in      
+    
+    $("input[name='multiLocationPlatform']").on('change',function() {
+  //  if (isCompPlatform == "Yes") {
+      if ($(this).val() == "Yes") {
+        document.getElementById("NoOfLocation").style.display = 'block';
+    } 
+ //   if (isCompPlatform == "No") {
+     if ($(this).val() == "No") {
+        document.getElementById("NoOfLocation").style.display = 'none';
+    }
+   });
 
+   });
 
 
 //fetch and select button --> Model js file --> Fetch comp list from DB
